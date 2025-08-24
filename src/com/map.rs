365ddc3a72dc82448b3d36731f01b31501860e49
soladmin <450220020@qq.com>
2025-08-24 19:20:44 +0800

    sequence fetching content

--- src/com/map.rs
+++ src/com/map.rs
@@ -12,6 +12,8 @@ static INSTANCE: OnceCell<DashMap<SingleKeyType, Box<SingleType>>> = OnceCell::n
 pub static INSTANCE_SINGLE: OnceCell<&DashMap<SingleKeyType, Box<SingleType>>> = OnceCell::new();
 
 
+
+
 #[macro_export]
 macro_rules! for_substring {
     ($str:expr,$first:expr,$last:expr) => {
@@ -29,9 +31,18 @@ macro_rules! get_map {
 #[macro_export]
 macro_rules! single_push {
     ($key:expr,$val:expr) => {
+        get_map!().entry($key.to_string()).insert($val)
+    };
+}
+
+#[macro_export]
+macro_rules! single_or_push {
+    ($key:expr,$val:expr) => {
         get_map!().entry($key.to_string()).or_insert($val)
     };
 }
+
+
 
 #[macro_export]
 macro_rules! single_get_unwrap {
@@ -44,8 +55,62 @@ macro_rules! single_get_unwrap {
     };
 }
 
+
+#[macro_export]
+macro_rules! single_get_str {
+    ($key:expr) => {
+        get_map!()
+            .get($key)
+            .unwrap()
+            .downcast_ref::<String>()
+            .unwrap().clone()
+    };
+}
+
+
+
+
 #[warn(dead_code)]
 pub fn single_init<'a>(
 ) -> &'a DashMap<SingleKeyType, Box<(dyn Any + Sync + std::marker::Send + 'a)>> {
     return INSTANCE.get_or_init(|| DashMap::new());
+}
+
+
+pub fn get_idx_val(key:&str)->String{
+    let key_idx_max_id = key.to_string()+"_max";
+    let key_idx_id = key.to_string()+"_sqe_idx";
+    let max_idx = single_get_unwrap!(&key_idx_max_id,i32);
+    let map_entity = get_map!();
+    if map_entity.contains_key(&key_idx_id){
+        map_entity.alter(&key_idx_id,  |_, v| {
+            let cur_id = *v.downcast_ref::<i32>().unwrap();
+            if max_idx>cur_id{
+                return Box::new(cur_id+1);
+            }else{
+                return Box::new(1);
+            }   
+        });
+    }else{
+        single_or_push!(key_idx_id,Box::new(1i32));
+    }
+ 
+    let cur_idx = single_get_unwrap!(&key_idx_id,i32);
+    let cur_key = key.to_string()+"_"+&cur_idx.to_string();
+    return single_get_str!(&cur_key);
+}
+
+
+#[macro_export]
+macro_rules! single_get_sqe_str {
+    ($key:expr) => {
+        $crate::com::map::get_idx_val($key)
+    };
+}
+
+#[macro_export]
+macro_rules! single_push_sql_max {
+    ($key:expr,$val:expr) => {
+        get_map!().entry($key.to_string()+"_max").insert($val)
+    };
 }
--- src/lib.rs
+++ src/lib.rs
@@ -9,4 +9,14 @@ fn test_map(){
     single_push!("a",Box::new("aaaa".to_string()));
     let straa = single_get_unwrap!("a",String);
     println!("rustl:{:?}",straa);
+    single_push_sql_max!("url",Box::new(3));
+    let na = "url_1";
+    single_push!(na,Box::new("url1111111".to_string()));
+    single_push!("url_2",Box::new("url1111111222222".to_string()));
+    single_push!("url_3",Box::new("url111111133333333".to_string()));
+    let a = single_get_sqe_str!("url");
+    let b = single_get_sqe_str!("url");
+    let c = single_get_sqe_str!("url");
+    let d = single_get_sqe_str!("url");
+    println!("a:{:?},b:{:?},c:{:?},d:{:?}",a,b,c,d);
 }
